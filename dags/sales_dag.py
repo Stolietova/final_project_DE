4ff17591aaeb57d80f032b978aa28496616ac827
Kateryna Stolietova <katerynastolietova@MacBook-Air-Kateryna.local>
2026-02-15 18:04:07 +0200

    refactor: rename main_dag.py to sales_dag.py for better clarity

--- dags/sales_dag.py
+++ dags/sales_dag.py
@@ -14,20 +14,18 @@ default_args = {
 
 with DAG(
     dag_id="process_sales_pipeline",
-    start_date=datetime(2024, 1, 1),
     schedule_interval=None,
     catchup=False,
 ) as dag:
 
-    # task_sales_raw_to_bronze = BashOperator(
-    #     task_id="process_sales_raw_to_bronze",
-    #     bash_command="python /opt/airflow/scripts/process_sales.py bronze"
-    # )
+    task_sales_raw_to_bronze = BashOperator(
+         task_id="sales_raw_to_bronze",
+         bash_command="python /opt/airflow/scripts/process_sales.py bronze"
+     )
 
     task_sales_bronze_to_silver = BashOperator(
-        task_id='bronze_to_silver',
+        task_id='sales_bronze_to_silver',
         bash_command='python /opt/airflow/scripts/process_sales.py silver'
     )
 
-    #task_sales_raw_to_bronze
-    task_sales_bronze_to_silver
+    task_sales_raw_to_bronze >> task_sales_bronze_to_silver
